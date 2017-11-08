77f86683f4a74c29f459aa6d486da9d3f4fb3ce5
tope018 <tope018@gmail.com>
2017-11-08 17:01:05 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Redbeardiki'
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = 'C:/Users/tope0_000/Desktop/Redbeardiki/content'
+USER_DIR = 'C:/Users/tope0_000/Desktop/Redbeardiki/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
